c30fb545fade42bfb5cb58fcf372d3d6379b9a64
Lăng Khải Tuấn <106717735+EOKAYTI@users.noreply.github.com>
2025-04-14 21:39:29 +0700

    init

---
+++ src/app/app.routes.ts
@@ -0,0 +1,9 @@
+// import { Routes } from '@angular/router';
+// import { HomeComponent } from './pages/Home/home.component';
+
+// export const routes: Routes = [
+//   {
+//     path: '/home',
+//     component: <HomeComponent />,
+//   },
+// ];
---
+++ src/app/pages/Home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css',
+})
+export class HomeComponent {
+  title = 'Angular';
+}
